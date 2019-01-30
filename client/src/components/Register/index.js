cfedf5b4c66fbde384fc8a361c8457f87074c1dd
Daniel Murphy <tugs.murphy@yahoo.com>
2019-01-29 19:51:04 -0500

    set up db, users routes, connection

---
+++ client/src/components/Register/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import API from "../../utils/API";
+// import { Link } from "react-router-dom";
+import { Input, FormBtn } from "../Form";
+
+import './register.css';
+
+
+
+class Users extends Component {
+    state = {
+        books: [],
+        name: "",
+        email: "",
+        password: ""
+      };
+
+      handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+      
+      handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.name && this.state.email && this.state.password) {
+          API.saveUser({
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+          })
+            .then(console.log(this.state.name + " is saved"))
+            // .then(res => this.loadBooks())
+            .catch(err => console.log(err));
+        }
+      };
+
+    // componentDidMount() {
+    //     fetch('/api/players')
+    //     .then(res => res.json())
+    //     .then(players => this.setState({players}, () => console.log("players fetched..", players)));
+    // }
+
+  render() {
+    return (
+      <div>
+        <h2>Register</h2>
+        <form>
+              <Input
+                value={this.state.name}
+                onChange={this.handleInputChange}
+                name="name"
+                placeholder="Name (required)"
+              />
+              <Input
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                name="email"
+                placeholder="Email (required)"
+              />
+              <Input
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                name="password"
+                placeholder="Password (required)"
+              />
+              <FormBtn
+                disabled={!(this.state.name && this.state.email && this.state.password)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Book
+              </FormBtn>
+            </form>
+      </div>
+    );
+  }
+}
+
+export default Users;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const routes = require("./routes");
+const db = require("./models")
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static("public"));
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  } else {
+    app.use(express.static("client/public"));
+  }
+
+// Routes
+app.use(routes);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "development") {
+    syncOptions.force = true;
+  }
+  
+
+
+const PORT = process.env.PORT || 5001;
+
+db.sequelize.sync(syncOptions).then(function() {
+  console.log("connected to DB")
+    app.listen(PORT, function() {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  });
+  
+//   module.exports = app;
