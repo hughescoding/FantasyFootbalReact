2d11d1b7dfef757029209f18ec4c55550d588e87
hughescoding <mhughes@myjao.org>
2019-02-02 11:11:03 -0500

    removed h1&2 added section and container to landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -13,11 +13,11 @@ class Landing extends Component {
           <section className="section">
           <img src="/images/logo-banner.jpg" alt="Logo"></img>
             <div className="container">
-              <h1 className="title">Title</h1>
+              {/* <h1 className="title">Title</h1>
               <h2 className="subtitle">
                 A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
                 <strong>A LOGO WILL GO HERE</strong>
-              </h2>
+              </h2> */}
             </div>
           </section>
           <section>
@@ -42,6 +42,9 @@ class Landing extends Component {
                   Join Gridiron Kings and begin your conquest
                 </p>
                 </div>
+                <section className="section"></section>
+                <div className="container"></div>
+                <h1>Line Goes Here</h1>
                 <footer className="card-footer">
                   <p className="card-footer-item">
                     <span>
