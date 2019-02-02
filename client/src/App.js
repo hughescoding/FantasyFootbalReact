1f794e24eac69a240e31111a8d454e84424d275d
Alyssa Templeton <templetonalyssa@gmail.com>
2019-02-02 14:52:08 -0500

    draft page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Landing from "./pages/Landing";
 import LockerRoom from "./pages/LockerRoom";
+import Draft from "./pages/Draft";
 import './components/navbar/navbar.css'
 
 class App extends Component {
@@ -15,7 +16,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route exact path="/LockerRoom" component={LockerRoom} />
-            {/* <Route exact path="/Home" component={Home} /> */}
+            <Route exact path="/Draft" component={Draft} />
             {/* <Route exact path="/MyProfile/:id?" component={MyProfile} /> */}
           </Switch>
         </Router>
